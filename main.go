6dffad965236b2f82e3178d93ad51f4fad06fa62
Michael Rivera <soymrmike@gmail.com>
2023-05-13 02:29:48 -0700

    Attempt to calculate rectangle of needle in the haystack space.

--- main.go
+++ main.go
@@ -13,11 +13,15 @@ package main
 import (
 	"os"
 	"fmt"
+	"math"
 	"image/color"
 	"gocv.io/x/gocv"
 )
 
 var w = 400
+const originEpsilon = 1
+const matchDistanceFactor = 0.70
+const expectedOriginRatio = 0.1
 
 func matchRender(needleImg gocv.Mat, needleKps []gocv.KeyPoint, hayStackImg gocv.Mat, hayStackKps []gocv.KeyPoint,
 	good []gocv.DMatch) gocv.Mat {
@@ -78,20 +82,62 @@ func main() {
 	matches := flannMatcher.KnnMatch(needleDesc, hayStackDesc, dontUnderstand)
 	fmt.Printf("Here we go: %p, number of matches is %d\n", matches, len(matches))
 
-	// dunno what this is
+	// dunno what this loop is doing. I know without it, we get too many bad matches.
 	var good []gocv.DMatch
 	for _, m := range matches {
 		if len(m) > 1 {
 			needleKp := needleKps[m[0].QueryIdx]
 			trainKp := hayStackKps[m[0].TrainIdx]
-			if m[0].Distance < 0.70 * m[1].Distance {
+			if m[0].Distance < matchDistanceFactor * m[1].Distance {
 				fmt.Printf("Hopefully a query key point (%.2f %.2f), train key point (%.2f, %.2f), and two distances: %.2f, %.2f, and image index of %d\n",
-					needleKp.X, needleKp.Y, trainKp.X, trainKp.Y, m[0].Distance, m[1].Distance, m[0].ImgIdx)
+					needleKp.X, needleKp.Y, trainKp.X, trainKp.Y, m[0].Distance, m[1].Distance, m[0].ImgIdx)			
 				good = append(good, m[0])
 			} else {
 				fmt.Printf("Bad query key point (%.2f %.2f), and two distances: %.2f, %.2f\n", needleKp.X, needleKp.Y, m[0].Distance, m[1].Distance)
 			}
 		}
+	}
+
+	// capture number of origins in the training image implied by the matches
+	var origins [][]float64
+	originCount := make(map[int]int)
+	for _, dMatch := range good {
+		needleKp := needleKps[dMatch.QueryIdx]
+		trainKp := hayStackKps[dMatch.TrainIdx]
+		trainOrigin := []float64{ trainKp.X - needleKp.X, trainKp.Y - needleKp.Y }
+
+		recognized := false
+		for _, origin := range origins {
+			if math.Abs(trainOrigin[0] - origin[0]) < originEpsilon && math.Abs(trainOrigin[1] - origin[1]) < originEpsilon {
+				recognized = true
+			}
+		}
+		
+		if !recognized {
+			origins = append(origins, []float64{ trainOrigin[0], trainOrigin[1] } )
+			originIdx := len(origins) - 1
+			if _, ok := originCount[originIdx]; !ok {
+				originCount[originIdx] = 0
+			}
+			originCount[originIdx] += 1
+		}		
+	}
+
+	// If there is at least one origin, and there aren't too many origins, pick the most popular one
+	foundOrigin := -1
+	if len(origins) >= 1 && (expectedOriginRatio * float64(len(good))) > float64(len(origins)) {
+		foundOrigin = 0
+		for originIdx, count := range originCount {
+			if count > originCount[foundOrigin] {
+				foundOrigin = originIdx
+			}
+		}
+	}
+
+	if foundOrigin != -1 {
+		fmt.Printf("There is a reasonably unique origin among %d origins\n", len(origins))
+		fmt.Printf("Origin in training image: (%.2f, %.2f, %.2f, %.2f)\n", origins[foundOrigin][0], origins[foundOrigin][1],
+			float64(needleImg.Cols()), float64(needleImg.Rows()))
 	}
 
 	out := matchRender(needleImg, needleKps, hayStackImg, hayStackKps, good)
